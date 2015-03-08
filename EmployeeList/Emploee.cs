63c9a755fff33c050c49a85d2ddb2d38df15fd4f
tashaozeruga <tashaozeruga@gmail.com>
2015-03-09 00:49:15 +0200

    this is commiting of the whole EmployeeList project

---
+++ EmployeeList/Emploee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeList
+{
+    class Emploee
+    {
+        int number;
+        string name;
+        string lastName;
+        int workHours;
+        int salary;
+
+        public Emploee(int number, string name, string lastName, int workHours)
+        {
+            this.number = number;
+            this.name = name;
+            this.lastName = lastName;
+            this.workHours = workHours;
+        }
+
+        public void printLine()
+        {
+            Console.WriteLine(
+                "| {0} | {1} |  {2} | {3} |",
+                number,
+                (name + " " + lastName).PadLeft(15),
+                workHours,
+                salary);
+              
+
+
+                
+        }
+
+    }
+}
---
+++ EmployeeList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Emploee> employees = new List<Emploee>();
+
+
+            employees.Add(new Emploee(1, "Alex", "Nuberg", 8));
+            employees.Add(new Emploee(2, "Vasya", "Ivanov", 7));
+            employees.Add(new Emploee(3, "Petya", "Petrov", 6));
+            employees.Add(new Emploee(4, "Kolya", "Sidorov", 5));
+            employees.Add(new Emploee(5, "Robert", "Lorry", 4));
+            employees.Add(new Emploee(6, "Bob", "Nolan", 3));
+
+            Console.WriteLine("| {0} | {1} | {2} | {3} |", "#", ("Name").PadLeft(15), "WH", "S");
+
+            foreach (Emploee emp in employees)
+            {
+                emp.printLine();
+            }
+
+        }
+    }
+}
